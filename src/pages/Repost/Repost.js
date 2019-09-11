a5428482583345223c399c5509e0a011e317958d
kcotias <kaianexperia@gmail.com>
2019-09-11 01:11:11 -0300

    finish repost function

--- src/pages/Repost/Repost.js
+++ src/pages/Repost/Repost.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { View, Image, Clipboard } from 'react-native';
+import { View, Image, Clipboard, Alert } from 'react-native';
 import GradientButton from '../../components/Button/Button';
 import Header from '../../components/Header/Header';
 
 const Repost = ({ navigation }) => {
   const thumbnail = navigation.getParam('thumbnail', '');
-  const title = navigation.getParam('title', '');
+  const title = navigation.getParam('description', '');
   return (
     <View>
       <Header hasBackButton onBackPress={() => navigation.goBack()} />
       <Image style={{ height: 300, margin: 10 }} source={thumbnail} />
       <GradientButton
         text="Repost"
-        onPress={() => Clipboard.setString(title)}
+        onPress={() => {
+          Clipboard.setString(title);
+          Alert.alert('Oba!', 'Texto copiado para o clipboard');
+        }}
       />
     </View>
   );
